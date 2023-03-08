c5bb637fee50ba57002e76050e8cafb80d1d909b
rewebd-solutions <127234723+rewebd-solutions@users.noreply.github.com>
2023-03-08 23:57:26 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ const swiper = new Swiper('.main-swiper', {
       prevEl: '.swiper-button-prev',
     },
   
-  });
+});
 
-  const projectsSwiper = new Swiper('.projects-swiper', {
+const projectsSwiper = new Swiper('.projects-swiper', {
     loop: true,
     slidesPerView: "auto",
     centeredSlides: true,
@@ -34,4 +34,26 @@ const swiper = new Swiper('.main-swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     }
+});
+
+var lastScrollTop = 0;
+
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop ; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop) {
+      document.querySelector(".nav-main").classList.add("nav-main-hide");
+   } else if (st < lastScrollTop) {
+      document.querySelector(".nav-main").classList.remove("nav-main-hide");
+   } // else was horizontal scroll
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+      });
   });
+});
