b1429208b531a30a291f9e857a856f6f495c63b5
rewebd-solutions <127234723+rewebd-solutions@users.noreply.github.com>
2023-03-08 19:35:09 +0530

    Add files via upload
    completed most of the site

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.main-swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
+    spaceBetween: 20,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: true,
+    },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true
     },
   
     // Navigation arrows
@@ -14,8 +19,19 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
+  });
+
+  const projectsSwiper = new Swiper('.projects-swiper', {
+    loop: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
   });
